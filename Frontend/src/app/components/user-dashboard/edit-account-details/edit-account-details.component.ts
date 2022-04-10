ddce4f812296060afad4b6cb1e2a327d9252412a
claudiu_badilas <Claudiu.Badilas@napa.fi>
2022-04-10 23:10:54 +0300

    try to improve form for account details

--- Frontend/src/app/components/user-dashboard/edit-account-details/edit-account-details.component.ts
+++ Frontend/src/app/components/user-dashboard/edit-account-details/edit-account-details.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { skipWhile } from 'rxjs/operators';
+import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
+import { AppState } from 'src/app/store/app.state';
+import * as fromPlatform from '../../../store/platform-state/platform.reducer';
 
 @Component({
   selector: 'app-edit-account-details',
@@ -6,7 +13,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-account-details.component.scss'],
 })
 export class EditAccountDetailsComponent implements OnInit {
-  constructor() {}
+  user$ = this.store.select(fromPlatform.getUser);
+  user: User = new User();
+  name = '';
+  email = '';
 
-  ngOnInit(): void {}
+  accountDetailsForm: FormGroup;
+
+  constructor(
+    private store: Store<AppState>,
+    private formBuilder: FormBuilder,
+    private userService: UserService
+  ) {}
+
+  ngOnInit(): void {
+    this.user$
+      .pipe(skipWhile((user: User) => user === null))
+      .subscribe((user) => {
+        this.name = user.firstName;
+        this.email = user.email;
+        this.user = user;
+      });
+    this.accountDetailsForm = this.formBuilder.group({
+      name: [
+        this.name,
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(30),
+        ],
+      ],
+      email: [
+        this.email,
+        [Validators.required, Validators.email, Validators.maxLength(100)],
+      ],
+    });
+  }
+
+  submitChanges(form) {
+    this.user = { ...this.user, firstName: form.name, email: form.email };
+  }
+
+  blur(): void {}
 }
